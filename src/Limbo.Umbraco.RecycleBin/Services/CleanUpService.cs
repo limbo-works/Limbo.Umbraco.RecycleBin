3bb67ab59ddb05855a3673c87f368121696d33a6
Thomas Nielsen <tni@limbo.works>
2023-06-09 10:25:59 +0200

    CleanUpService CleanUpContent

--- src/Limbo.Umbraco.RecycleBin/Services/CleanUpService.cs
+++ src/Limbo.Umbraco.RecycleBin/Services/CleanUpService.cs
@@ -1,20 +1,45 @@
 ﻿using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
 
 namespace Limbo.Umbraco.RecycleBin.Services {
     public class CleanUpService {
 
         private readonly ILogger<CleanUpService> _logger;
+        private readonly IContentService _contentService;
 
-        public CleanUpService(ILogger<CleanUpService> logger) {
+        public CleanUpService(ILogger<CleanUpService> logger, IContentService contentService) {
             _logger = logger;
+            _contentService = contentService;
         }
 
         internal void CleanUpContent() {
-            _logger.LogInformation("CleanUpContent");
+            _logger.LogInformation("CleanUpContent Begin");
+
+            try {
+                if (!_contentService.RecycleBinSmells()) {
+                    return;
+                }
+
+                IEnumerable<IContent> items = _contentService.GetPagedContentInRecycleBin(0, int.MaxValue, out long totalRecords);
+                foreach (IContent item in items) {
+                    if ((DateTime.Now - item.UpdateDate).Days > 14) {
+                        try {
+                            _logger.LogInformation("Deleted: " + item.Name);
+                            _contentService.Delete(item);
+                        } catch {
+                        }
+                    }
+                }
+            } catch {
+            }
+
+            _logger.LogInformation("CleanUpContent End");
         }
 
         internal void CleanUpMedia() {
-            _logger.LogInformation("CleanUpMedia");
+            _logger.LogInformation("CleanUpMedia Begin");
+            _logger.LogInformation("CleanUpMedia End");
         }
     }
 }
