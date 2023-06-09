1c24fe8f8ebb5cdcae598f68579adeb6a26250e4
Thomas Nielsen <tni@limbo.works>
2023-06-09 18:12:46 +0200

    MediaMovedNotificationHandler + MediaMovedToRecycleBinNotificationHandler

--- src/Limbo.Umbraco.RecycleBin/NotificationHandlers/MediaMovedNotificationHandler.cs
+++ src/Limbo.Umbraco.RecycleBin/NotificationHandlers/MediaMovedNotificationHandler.cs
@@ -1,19 +1,45 @@
 ﻿using Microsoft.Extensions.Logging;
 using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.IO;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Extensions;
 
 namespace Limbo.Umbraco.RecycleBin.NotificationHandlers {
     public class MediaMovedNotificationHandler : INotificationHandler<MediaMovedNotification> {
 
         private readonly ILogger<MediaMovedNotificationHandler> _logger;
+        private readonly MediaFileManager _mediaFileManager;
 
-        public MediaMovedNotificationHandler(ILogger<MediaMovedNotificationHandler> logger) {
+        public MediaMovedNotificationHandler(ILogger<MediaMovedNotificationHandler> logger, MediaFileManager mediaFileManager) {
             _logger = logger;
+            _mediaFileManager = mediaFileManager;
         }
 
         public void Handle(MediaMovedNotification notification) {
             foreach (var mediaItem in notification.MoveInfoCollection) {
+
                 _logger.LogInformation("Moved " + mediaItem.Entity.Id.ToString());
+
+                try {
+
+                    var filePath = mediaItem.Entity.GetValue<string>("umbracoFile");
+                    if (filePath == null) {
+                        return;
+                    }
+
+                    var fileExists = _mediaFileManager.FileSystem.FileExists(filePath + ".deleted");
+                    if (!fileExists) {
+                        return;
+                    }
+
+                    _mediaFileManager.FileSystem.CopyFile(filePath + ".deleted", filePath);
+                    _mediaFileManager.FileSystem.DeleteFile(filePath + ".deleted");
+
+                } catch {
+
+                }
+
+
             }
         }
 
--- src/Limbo.Umbraco.RecycleBin/NotificationHandlers/MediaMovedToRecycleBinNotificationHandler.cs
+++ src/Limbo.Umbraco.RecycleBin/NotificationHandlers/MediaMovedToRecycleBinNotificationHandler.cs
@@ -1,19 +1,42 @@
 ﻿using Microsoft.Extensions.Logging;
 using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.IO;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Extensions;
 
 namespace Limbo.Umbraco.RecycleBin.NotificationHandlers {
     public class MediaMovedToRecycleBinNotificationHandler : INotificationHandler<MediaMovedToRecycleBinNotification> {
 
         private readonly ILogger<MediaMovedToRecycleBinNotificationHandler> _logger;
+        private readonly MediaFileManager _mediaFileManager;
 
-        public MediaMovedToRecycleBinNotificationHandler(ILogger<MediaMovedToRecycleBinNotificationHandler> logger) {
+        public MediaMovedToRecycleBinNotificationHandler(ILogger<MediaMovedToRecycleBinNotificationHandler> logger, MediaFileManager mediaFileManager) {
             _logger = logger;
+            _mediaFileManager = mediaFileManager;
         }
 
         public void Handle(MediaMovedToRecycleBinNotification notification) {
             foreach (var mediaItem in notification.MoveInfoCollection) {
-                _logger.LogInformation("MovedToRecycleBin " + mediaItem.Entity.Id.ToString());
+
+                try {
+
+                    var filePath = mediaItem.Entity.GetValue<string>("umbracoFile");
+                    if (filePath == null) {
+                        return;
+                    }
+
+                    var fileExists = _mediaFileManager.FileSystem.FileExists(filePath);
+                    if (!fileExists) {
+                        return;
+                    }
+
+                    _mediaFileManager.FileSystem.CopyFile(filePath, filePath + ".deleted");
+                    _mediaFileManager.FileSystem.DeleteFile(filePath);
+
+                } catch {
+
+                }
+
             }
         }
 
