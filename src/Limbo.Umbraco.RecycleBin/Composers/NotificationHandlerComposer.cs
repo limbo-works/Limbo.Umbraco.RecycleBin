5bf3847f814826a79b168a026c546bab5ef54496
Thomas Nielsen <tni@limbo.works>
2023-06-09 18:14:02 +0200

    CleanUp

--- src/Limbo.Umbraco.RecycleBin/Composers/NotificationHandlerComposer.cs
+++ src/Limbo.Umbraco.RecycleBin/Composers/NotificationHandlerComposer.cs
@@ -7,9 +7,7 @@ namespace Limbo.Umbraco.RecycleBin.Composers {
     public class NotificationHandlerComposer : IComposer {
 
         public void Compose(IUmbracoBuilder builder) {
-            builder.AddNotificationHandler<MediaMovingNotification, MediaMovingNotificationHandler>();
             builder.AddNotificationHandler<MediaMovedNotification, MediaMovedNotificationHandler>();
-            builder.AddNotificationHandler<MediaMovingToRecycleBinNotification, MediaMovingToRecycleBinNotificationHandler>();
             builder.AddNotificationHandler<MediaMovedToRecycleBinNotification, MediaMovedToRecycleBinNotificationHandler>();
             builder.AddNotificationHandler<MediaDeletingNotification, MediaDeletingNotificationHandler>();
             builder.AddNotificationHandler<MediaDeletedNotification, MediaDeletedNotificationHandler>();
