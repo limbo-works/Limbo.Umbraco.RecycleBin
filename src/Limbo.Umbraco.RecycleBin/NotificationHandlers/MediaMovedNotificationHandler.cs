14b81aa1cf89574059bdbdf7f653caddeaf802c3
Thomas Nielsen <tni@limbo.works>
2023-06-09 17:27:05 +0200

    MediaMovingNotificationHandler + MediaMovedNotificationHandler

---
+++ src/Limbo.Umbraco.RecycleBin/NotificationHandlers/MediaMovedNotificationHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+
+namespace Limbo.Umbraco.RecycleBin.NotificationHandlers {
+    public class MediaMovedNotificationHandler : INotificationHandler<MediaMovedNotification> {
+
+        private readonly ILogger<MediaMovedNotificationHandler> _logger;
+
+        public MediaMovedNotificationHandler(ILogger<MediaMovedNotificationHandler> logger) {
+            _logger = logger;
+        }
+
+        public void Handle(MediaMovedNotification notification) {
+            foreach (var mediaItem in notification.MoveInfoCollection) {
+                _logger.LogInformation("Moved " + mediaItem.Entity.Id.ToString());
+            }
+        }
+
+    }
+}
