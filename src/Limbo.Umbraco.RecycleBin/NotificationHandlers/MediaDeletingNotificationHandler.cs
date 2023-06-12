7b6ad43857f8685d39928b38bd348f81d6fceebb
Thomas Nielsen <tni@limbo.works>
2023-06-12 08:20:07 +0200

    MediaDeletingNotificationHandler

--- src/Limbo.Umbraco.RecycleBin/NotificationHandlers/MediaDeletingNotificationHandler.cs
+++ src/Limbo.Umbraco.RecycleBin/NotificationHandlers/MediaDeletingNotificationHandler.cs
@@ -1,19 +1,44 @@
 ﻿using Microsoft.Extensions.Logging;
 using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.IO;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Extensions;
 
 namespace Limbo.Umbraco.RecycleBin.NotificationHandlers {
     public class MediaDeletingNotificationHandler : INotificationHandler<MediaDeletingNotification> {
 
         private readonly ILogger<MediaDeletingNotificationHandler> _logger;
+        private readonly MediaFileManager _mediaFileManager;
 
-        public MediaDeletingNotificationHandler(ILogger<MediaDeletingNotificationHandler> logger) {
+        public MediaDeletingNotificationHandler(ILogger<MediaDeletingNotificationHandler> logger, MediaFileManager mediaFileManager) {
             _logger = logger;
+            _mediaFileManager = mediaFileManager;
         }
 
         public void Handle(MediaDeletingNotification notification) {
             foreach (var mediaItem in notification.DeletedEntities) {
+
                 _logger.LogInformation("Deleting " + mediaItem.Id.ToString());
+
+                try {
+
+                    var filePath = mediaItem.GetValue<string>("umbracoFile");
+                    if (filePath == null) {
+                        return;
+                    }
+
+                    var fileExists = _mediaFileManager.FileSystem.FileExists(filePath + ".deleted");
+                    if (!fileExists) {
+                        return;
+                    }
+
+                    _mediaFileManager.FileSystem.CopyFile(filePath + ".deleted", filePath);
+                    _mediaFileManager.FileSystem.DeleteFile(filePath + ".deleted");
+
+                } catch {
+
+                }
+
             }
         }
 
