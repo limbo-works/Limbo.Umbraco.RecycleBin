eb7c3ca3513a2289b2ab1b07db89dace7a18fcd1
Anders Bjerner <abj@limbo.works>
2025-10-20 09:44:34 +0200

    Configuration properties should have public setters
    This ensures that we can update the configuration via code in our individual projects.

--- src/Limbo.Umbraco.RecycleBin/Settings/CleanUpSettings.cs
+++ src/Limbo.Umbraco.RecycleBin/Settings/CleanUpSettings.cs
@@ -1,9 +1,9 @@
 ﻿namespace Limbo.Umbraco.RecycleBin.Settings {
     public class CleanUpSettings {
 
-        public bool Enabled { get; internal set; } = false;
+        public bool Enabled { get; set; } = false;
 
-        public int DeleteAfterDays { get; internal set; } = 30;
+        public int DeleteAfterDays { get; set; } = 30;
 
     }
 }
--- src/Limbo.Umbraco.RecycleBin/Settings/CleanUpSettingsRecycleBin.cs
+++ src/Limbo.Umbraco.RecycleBin/Settings/CleanUpSettingsRecycleBin.cs
@@ -1,9 +1,9 @@
 ﻿namespace Limbo.Umbraco.RecycleBin.Settings {
     public class CleanUpSettingsRecycleBin {
 
-        public CleanUpSettings Content { get; internal set; }
+        public CleanUpSettings Content { get; set; } = new();
 
-        public CleanUpSettings Media { get; internal set; }
+        public CleanUpSettings Media { get; set; } = new();
 
     }
 }
